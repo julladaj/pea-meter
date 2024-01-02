feb934148f19f92f12d2054b524baba88a8e9ff9
Julladaj Bleriot <joe@data-horizon.com>
2024-01-02 08:21:15 +0700

    Refactoring class

--- public/class/database.php
+++ public/class/database.php
@@ -2,23 +2,19 @@
 
 class Database
 {
-    var $mysqli;
+    public $mysqli;
 
-    function __construct()
+    public function __construct()
     {
         $this->mysqli = new mysqli(DB_HOSTNAME, DB_USERNAME, DB_PASSWORD, DB_DATABASE);
 
         if ($this->mysqli->connect_errno) {
             die("Connect failed: " . $this->mysqli->connect_error);
-            return;
         }
 
         if (!$this->mysqli->set_charset("utf8")) {
             die("Error loading character set utf8: " . $this->mysqli->error);
-            return;
         }
         mysqli_set_charset($this->mysqli, "utf8");
     }
 }
-
-?>
